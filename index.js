48b47b7d1ae67f9817d6ad06ee0b7d843ca4fe60
lukew244 <luke.webster86@gmail.com>
2016-09-22 16:50:03 +0100

    exercise

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello!");
+function CountModel() {
+  this._count = 0;
+};
+
+CountModel.prototype = {
+  count: function() {
+    return this._count;
+  },
+
+  set: function(count) {
+    this._count = count;
+  }
+};
+
+var countModel = new CountModel();
+
+// What happens if you add this code? Why?
+countModel.set = function() {
+  return "hello";
+};
+
+countModel.set(5);
+
+console.log("count is", countModel.count());
